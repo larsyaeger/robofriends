5d61e027e87ad720db774670f1d9fea24115c5d0
larsyaeger <larsyaeger@gmail.com>
2023-02-01 17:56:11 -0600

    componentdidmount now fetches users from a url rather than having robots objects baked into code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Cardlist from "./Cardlist";
-import { robots } from './robots';
 import Searchbox from './Searchbox';
-import './App.css'
+import './App.css';
 
 
 
@@ -10,22 +9,32 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     };
   }
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})  
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
   render() {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
+    });
     return (
       <div className="tc">
         <h1 className='f1'>RoboFriends</h1>
-        <Searchbox searchChange={this.onSearchChange}/>
+        <Searchbox searchChange={this.onSearchChange} />
         <Cardlist robots={filteredRobots} />
       </div>
     );
